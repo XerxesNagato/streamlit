c3f74fc922503509e20e707373c078066fff4335
Nissay <XerxesNagato@users.noreply.github.com>
2023-04-28 23:23:02 +0100

    Update second.py

--- second.py
+++ second.py
@@ -11,14 +11,14 @@ dis_link = 'https://discord.gg/v3FDUQ5t'
 
 st.header('The Squad')
 #st.image('logo.png')
-dis_btn = st.button('Discord')
+dis_btn = st.sidebar.button('Discord')
 if dis_btn:
     webbrowser.open_new_tab(dis_link)
 
-st.header('Members:')
-st.write('Nagat__0#')
-st.write('Wahio')
-st.write('BloodyShirou')
-st.write('Frost')
-st.write('Kail0o#')
-st.write('Silver')
+st.sidebar.header('Members:')
+st.sidebar.write('Nagat__0#')
+st.sidebar.write('Wahio')
+st.sidebar.write('BloodyShirou')
+st.sidebar.write('Frost')
+st.sidebar.write('Kail0o#')
+st.sidebar.write('Silver')
