4dae9df6672b449120df6a33d89bc972fc15093a
Nissay <XerxesNagato@users.noreply.github.com>
2023-04-28 22:07:23 +0100

    Add files via upload

---
+++ second.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+import pycountry
+import webbrowser
+
+
+
+dis_link = 'https://discord.gg/v3FDUQ5t'
+
+st.title('The Squad')
+dis_btn = st.button('Discord')
+if dis_btn:
+    webbrowser.open_new_tab(dis_link)
+
+st.header('Members:')
+st.write('Nagat__0#')
+st.write('Wahio')
+st.write('BloodyShirou')
+st.write('Frost')
+st.write('Kail0o#')
+st.write('Silver')
