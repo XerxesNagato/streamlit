fad1355e0900e0de89c5c6040a6f57295afa1adb
Nissay <XerxesNagato@users.noreply.github.com>
2023-04-29 18:48:14 +0100

    Update second.py

--- second.py
+++ second.py
@@ -15,10 +15,7 @@ dis_link = 'https://discord.gg/v3FDUQ5t'
 
 st.header('The Squad')
 #st.image('logo.png')
-dis_btn = st.sidebar.button(f'''
-<a href={dis_link}><button style="background-color:blue;">Discord</button></a>
-''',
-unsafe_allow_html=True)
+dis_btn = st.sidebar.button(f'<a href={dis_link}><button style="background-color:blue;">Discord</button></a>',unsafe_allow_html=True)
 # if dis_btn:
 #         st.markdown(f'''
 # <a href={dis_link}><button style="background-color:GreenYellow;">Discord</button></a>
